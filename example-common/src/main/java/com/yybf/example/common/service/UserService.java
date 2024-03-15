9ab35bb160a0f90f8f02244acb5a0bd20be508ef
YangYi <1134456853@qq.com>
2024-03-15 21:34:20 +0800

    完成mock服务的测试环境搭建

--- example-common/src/main/java/com/yybf/example/common/service/UserService.java
+++ example-common/src/main/java/com/yybf/example/common/service/UserService.java
@@ -15,6 +15,18 @@ public interface UserService {
      * @return com.yybf.example.common.model.User:
      * @author yangyibufeng
      * @date 2024/3/11 13:37
-     */  
+     */
     User getUser(User user);
+
+    /**
+     * 获取一个固定数字
+     * 用来区分是否调用Mock接口
+     *
+     * @return int:
+     * @author yangyibufeng
+     * @date 2024/3/15 21:27
+     */
+    default int getNumber() {
+        return 114514;
+    }
 }
--- example-consumer/src/main/java/com/yybf/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yybf/example/consumer/ConsumerExample.java
@@ -2,7 +2,10 @@ package com.yybf.example.consumer;
 
 import cn.hutool.core.collection.SpliteratorUtil;
 import com.yybf.chenrpc.config.RpcConfig;
+import com.yybf.chenrpc.proxy.ServiceProxyFactory;
 import com.yybf.chenrpc.utils.ConfigUtil;
+import com.yybf.example.common.model.User;
+import com.yybf.example.common.service.UserService;
 
 /**
  * 扩展RPC后示例消费者类，测试配置文件的读取
@@ -12,6 +15,29 @@ import com.yybf.chenrpc.utils.ConfigUtil;
  */
 public class ConsumerExample {
     public static void main(String[] args) {
+        testReadConfig();
+    }
+
+    public static void testUsingMock() {
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("yybf");
+
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+
+        // 验证是否使用Mock
+        long num = userService.getNumber();
+        System.out.println("num = " + num);
+    }
+
+    private static void testReadConfig() {
         RpcConfig rpcConfig = ConfigUtil.loadConfig(RpcConfig.class,"rpc");
         System.out.println(rpcConfig);
     }
