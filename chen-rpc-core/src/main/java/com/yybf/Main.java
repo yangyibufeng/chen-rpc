0214e992e9dc48fbc00a06354fcf0ca9292f8e82
YangYi <1134456853@qq.com>
2024-03-15 17:25:44 +0800

    添加chen-rpc-core模块，作为后续项目开发以及扩展的模块；并将consumer和provider的rpc依赖替换为新增模块

---
+++ chen-rpc-core/src/main/java/com/yybf/Main.java
@@ -0,0 +1,12 @@
+package com.yybf;
+
+/**
+ * @author yangyibufeng
+ * @Description
+ * @date 2024/3/15-17:22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
