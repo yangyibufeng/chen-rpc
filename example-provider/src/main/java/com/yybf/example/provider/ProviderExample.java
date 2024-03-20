8deab1c1b53de441a849fe4781144be6923b8a90
YangYi <1134456853@qq.com>
2024-03-20 21:22:51 +0800

    将服务提供者示例添加注册服务到注册中心的机制

--- example-provider/src/main/java/com/yybf/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/yybf/example/provider/ProviderExample.java
@@ -1,7 +1,12 @@
 package com.yybf.example.provider;
 
 import com.yybf.chenrpc.RpcApplication;
+import com.yybf.chenrpc.config.RegistryConfig;
+import com.yybf.chenrpc.config.RpcConfig;
+import com.yybf.chenrpc.model.ServiceMetaInfo;
 import com.yybf.chenrpc.registry.LocalRegistry;
+import com.yybf.chenrpc.registry.Registry;
+import com.yybf.chenrpc.registry.RegistryFactory;
 import com.yybf.chenrpc.server.HttpServer;
 import com.yybf.chenrpc.server.VertxHttpServer;
 import com.yybf.example.common.service.UserService;
@@ -19,7 +24,25 @@ public class ProviderExample {
         RpcApplication.init();
 
         // 注册服务
-        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
+        String serviceName = UserService.class.getName();
+        LocalRegistry.register(serviceName,UserServiceImpl.class);
+
+        // 注册服务到注册中心
+        // 获取一个注册中心的实例
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        // 构建服务信息
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
+        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+        try {
+            // 注册
+            registry.register(serviceMetaInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // 启动web服务
         HttpServer httpServer = new VertxHttpServer();
