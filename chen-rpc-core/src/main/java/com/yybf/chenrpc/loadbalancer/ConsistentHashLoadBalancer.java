719a0418d09947f110eaff8b1c28141bc9e58bd4
YangYi <1134456853@qq.com>
2024-04-15 19:11:54 +0800

    完善注释

--- chen-rpc-core/src/main/java/com/yybf/chenrpc/loadbalancer/ConsistentHashLoadBalancer.java
+++ chen-rpc-core/src/main/java/com/yybf/chenrpc/loadbalancer/ConsistentHashLoadBalancer.java
@@ -45,7 +45,8 @@ public class ConsistentHashLoadBalancer implements LoadBalancer {
         // 获取调用请求的hash值
         int hash = getHash(requestParams);
         /*这里如果是同一个请求的话，因为requestParams传入的参数是调用的函数方法名（getMethodName），
-          所以对于同一个方法这里计算出的hash值总是一样的，也就会造成调用相同的方法总是会请求到相同的服务节点上面*/
+          所以对于同一个方法这里计算出的hash值总是一样的，也就会造成调用相同的方法总是会请求到相同的服务节点上面
+          可以通过修改hash算法 --》 gethash 来通过其他不同的特征计算出hash值，来实现不同节点间的负载均衡*/
 
         // 选择最接近且大于等于调用请求的hash值的虚拟节点，如果没有就返回环首部的节点
         Map.Entry<Integer, ServiceMetaInfo> entry = virtualNodes.ceilingEntry(hash);
